8d8b5d31af0903f9f8a56c4aae9807d1b1ce1eec
m2m tech <tech@m2m.at>
2013-12-05 14:57:31 +0100

    Initial Commit

---
+++ TopPlaces/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TopPlaces
+//
+//  Created by Martin Mandl on 05.12.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TopPlacesTests/TopPlacesTests.m
@@ -0,0 +1,34 @@
+//
+//  TopPlacesTests.m
+//  TopPlacesTests
+//
+//  Created by Martin Mandl on 05.12.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TopPlacesTests : XCTestCase
+
+@end
+
+@implementation TopPlacesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
